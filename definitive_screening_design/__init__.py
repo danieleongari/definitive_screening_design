58dda0762c3271d495b1bb6a08c893fcb2643bad
Daniele Ongari <daniele.ongari@gmail.com>
2022-09-19 19:27:22 +0200

    Use, by default, fake factors if less than 6 continuous factors

--- definitive_screening_design/__init__.py
+++ definitive_screening_design/__init__.py
@@ -1,5 +1,6 @@
 """All the functions you need are in this init module."""
 
+from tkinter import N
 import pandas as pd
 import numpy as np
 from pathlib import Path
@@ -38,12 +39,49 @@ def create(factors) -> pd.DataFrame:
 
     return df
 
-def generate(n_cont_factors, n_cat_factors, method='dsd'):
-    """Generate DSD with 2-levels categoricals design from calculation (Jones 2013)."""
-    return pd.DataFrame(
-        compute_dsd(n_cont_factors,n_cat_factors,method),
-        columns=[ f"X{i:02d}" for i in range(1, n_cont_factors+1) ] + [ f"C{i:02d}" for i in range(1, n_cat_factors+1) ]
-    )
+def generate(n_cont_factors, n_cat_factors, method='dsd', min_trials=True) -> pd.DataFrame:
+    """Generate DSD with 2-levels categoricals design from calculation (Jones 2013).
+    
+    INPUTS
+
+        n_cont_factors (int)
+            Number of continuous factors.
+
+        n_cat_factors (int)
+            Number of 2-levels categorical factors.
+
+        method (str)
+            Design choice: 'dsd' for de-alias all two-factor interactions with
+            categorical factors or 'orth' to make orthogonal main-effects plan.
+
+        min_trials (bool)
+            If True, augment the experiments with 2, 3, 4 and 5 numerical factors
+            to at least 13 trials.
+
+    OUTPUTS
+
+        dsd_df (pandas.DataFrame)
+            Table with DSD trials. Order is not randomized.
+
+    """
+    assert n_cont_factors>1, "DSD must have at least 2 numerical factors"
+
+    fake_factors = 0 # Fake continuous factor used to augment the number of trials
+    if min_trials and n_cont_factors<6:
+        fake_factors = 6-n_cont_factors
+
+    dsd_array = compute_dsd(n_cont_factors+fake_factors, n_cat_factors, method)
+
+    # Delete columns of fake factor
+    for i in range(fake_factors):
+        dsd_array = np.delete(dsd_array, n_cont_factors, 1)
+
+    column_labels = [ f"X{i:02d}" for i in range(1, n_cont_factors+1) ] + [ f"C{i:02d}" for i in range(1, n_cat_factors+1) ]
+    index_labels = [ x+1 for x in range(dsd_array.shape[0])]
+
+    dsd_df = pd.DataFrame(dsd_array, columns=column_labels, index=index_labels)
+
+    return dsd_df
 
 
 
